3dc72adf99932fcacd58e22e577243e046e900c0
mofosyne <mofosyne@gmail.com>
2015-08-29 00:54:32 +1000

    V1.2
    This time increased user friendlyness of interface. Added dumb append
    scanning for sequence of QR codes. Also added framework to make smarter
    structured appends, and perhaps a direct binary mode to avoid having to
    use base64 (with has 30% overhead).

--- app/src/main/java/com/github/mofosyne/tagdrop/main.java
+++ app/src/main/java/com/github/mofosyne/tagdrop/main.java
@@ -4,28 +4,38 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.webkit.WebChromeClient;
-import android.webkit.WebSettings;
-import android.webkit.WebView;
-import android.webkit.WebViewClient;
-import android.widget.Button;
 import android.widget.TextView;
 
 public class main extends AppCompatActivity {
+    //
+
+    //
     TextView debugDisp;
 
-    String datauriString;
+    // Related to structured append of seqences of QR codes
+    boolean structuredAppendDetected;
+    int currentSeqNum;
+    int totalInSeqNum;
+    String hashtype;
+    String hashcheck;
+    // Related to string based structured appends
+    String datauriString; // Stores the full data uri string to parse
+    // todo: Related to binary based string append.
 
-    Button scanButton;
+    String[] contentArray; //  Stores sequences
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // On load, clear everything
+        clear();
 
         /*
         Intent iObj = getIntent();
@@ -90,13 +100,6 @@ public class main extends AppCompatActivity {
     // Does this work to parse dataUri??? I wonder... ANS: NOOOPE... this just replaces "data:" with "http:"
     public void openWebPage(String url) {
         Uri webpage = Uri.parse(url);
-
-        /*
-        Uri.Builder httpDataUri = webpage.buildUpon();
-        httpDataUri.scheme("http");
-        webpage = httpDataUri.build();
-        */
-
         Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
         if (intent.resolveActivity(getPackageManager()) != null) {
             startActivity(intent);
@@ -109,41 +112,92 @@ public class main extends AppCompatActivity {
     *   For zxing loading
     * */
 
+    static int QR_READER_ACTIVITY_REQUEST_CODE=0;
+
     public void scanButton(View v) { // You must have "View v" or app will crash
+        launchBarcodeReader("Scan first code. Press back/return to escape. ");
+    }
+
+    public void launchBarcodeReader( String promptMessage ) {
         Intent intent = new Intent("com.google.zxing.client.android.SCAN");
         intent.putExtra("SCAN_MODE", "QR_CODE_MODE");
-        startActivityForResult(intent, 0);
+        intent.putExtra("SCAN_FORMATS", "DATA_MATRIX,QR_CODE,MAXICODE,PDF_417,AZTEC");
+        intent.putExtra("PROMPT_MESSAGE", promptMessage );
+        startActivityForResult(intent, QR_READER_ACTIVITY_REQUEST_CODE);
+    }
+
+    public void clearButton(View v) { // You must have "View v" or app will crash
+        clear();
     }
 
 
+
     public void onActivityResult(int requestCode, int resultCode, Intent intent) {
-        if (requestCode == 0) {
+        if (requestCode == QR_READER_ACTIVITY_REQUEST_CODE) {
             if (resultCode == RESULT_OK) {
                 String contents = intent.getStringExtra("SCAN_RESULT");
                 String format = intent.getStringExtra("SCAN_RESULT_FORMAT");
                 String bytecount = intent.getStringExtra("SCAN_RESULT_BYTES");
                 String orientation = intent.getStringExtra("SCAN_RESULT_ORIENTATION");
                 String eccLvl = intent.getStringExtra("SCAN_RESULT_ERROR_CORRECTION_LEVEL");
+                String strExtList = intent.getExtras().toString();
                 // Handle successful scan
-
-                datauriString = contents;
-
                 String debugText =
                         "DEBUG VIEW\n"
-                                + "Content: '" + datauriString + "'\n"
-                                + ", format:'" + format + "'\n"
-                                + ", bytecount:'" + bytecount + "'\n"
-                                + ", orientation:'" + orientation + "'\n"
-                                + ", eccLvl:'" + eccLvl + "'\n"
+                                + "Content: '" + datauriString + "'\n\n"
+                                + ", format:'" + format + "'\n\n"
+                                + ", bytecount:'" + bytecount + "'\n\n"
+                                + ", orientation:'" + orientation + "'\n\n"
+                                + ", eccLvl:'" + eccLvl + "'\n\n"
+                                + ", Other Strings Extras:'" + strExtList + "'\n\n"
                         ; // Can we get structured append metadata here?
                 debugDisp = (TextView) findViewById(R.id.debugView);
                 debugDisp.setText( debugText );
                 Log.d("incoming intent", debugText);
 
+                // Save and append if required
+                if(structuredAppendDetected) {
+                    //todo: logic on smart addition of qr content (Then we could auto launch when all is received.
+                    contentArray[currentSeqNum] = contents;
+                } else {
+                    if (false) {
+                        //todo: would be good to autodetect datauris and respond as if it was a new sequence
+                    } else {
+                        // Dumb append mode
+                        contentArray[currentSeqNum] = contents;
+                        currentSeqNum++;
+                        totalInSeqNum++;
 
+                        // Ask for next sequence
+                        launchBarcodeReader("Scanning Next Sequence. Press back/return to escape. ");
+                    }
+                }
             } else if (resultCode == RESULT_CANCELED) {
                 // Handle cancel
             }
         }
+
+        // Update Preview Screen
+        datauriString = TextUtils.join( "", contentArray ); // todo: need to concat all the arrays together
+        debugDisp = (TextView) findViewById(R.id.debugView);
+        debugDisp.setText( datauriString );
+
+    }
+
+    //-------------------------------------------------------
+    public void clear(){
+        structuredAppendDetected = false;
+        currentSeqNum = 0;
+        totalInSeqNum = 1;
+        hashtype = "none";
+        hashcheck = "";
+        datauriString = "";
+        contentArray = new String[50]; // For now, just hardcode the amount to 256, which is more than enough for anyone
+        for(int i = 0; i < contentArray.length; i++){
+            contentArray[i]="";
+        }
+        // Don't forget to reset the display
+        debugDisp = (TextView) findViewById(R.id.debugView);
+        debugDisp.setText( "" );
     }
 }
